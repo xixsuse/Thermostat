b1daadbae75b882c3206405025b85f0f9aad78a8
stefanberg96 <bergshm@gmail.com>
2015-06-13 15:19:37 +0200

    added circular seekbar

---
+++ app/src/main/java/htigroup18/thermostat/CircleSeekBarListener.java
@@ -0,0 +1,19 @@
+package htigroup18.thermostat;
+
+import com.devadvance.circularseekbar.CircularSeekBar;
+
+public class CircleSeekBarListener implements CircularSeekBar.OnCircularSeekBarChangeListener {
+    @Override
+    public void onProgressChanged(CircularSeekBar circularSeekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(CircularSeekBar seekBar) {
+
+    }
+    @Override
+    public void onStartTrackingTouch(CircularSeekBar bar) {
+
+    }
+}
